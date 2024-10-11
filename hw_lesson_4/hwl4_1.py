b5363e545b05bc09fdac811d53feb62323854f3e
easyhardovich <easyhardovich@gmail.com>
2024-10-11 18:17:43 +0300

    doing home work for lesson 4.1

---
+++ hw_lesson_4/hwl4_1.py
@@ -0,0 +1,8 @@
+mylist = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
+i = 0
+while i < len(mylist):
+    if mylist[i] == 0:
+        mylist.append(mylist[i])
+        mylist.remove(mylist[i])
+    i += 1
+print(mylist)
