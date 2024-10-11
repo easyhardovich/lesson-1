b9fe6561c0b6098dfc26bd3bbce334c85d3cfdd6
easyhardovich <easyhardovich@gmail.com>
2024-10-11 20:53:19 +0300

    doing home work for lesson 4.3

---
+++ hw_lesson_4/hwl4_3.py
@@ -0,0 +1,18 @@
+# Створіть список випадкових чисел із випадковою кількістю елементів від 3 до 10.
+# Ваше завдання - створити новий список з 3 елементів початкового списку - першим, третім і другим з кінця.
+import random
+
+firstlist = []
+# наповнюємо перший список
+i = 0
+while i < random.randint(3, 10):
+    firstlist.append(random.randint(0, 9))
+    i += 1
+# створюємо новий список
+secondlist = []
+# наповнюємо другий список трьома елементами початкового списку - першим, третім і другим з кінця
+secondlist.append(firstlist[0])
+secondlist.append(firstlist[2])
+secondlist.append(firstlist[-2])
+print(firstlist)
+print(secondlist)
