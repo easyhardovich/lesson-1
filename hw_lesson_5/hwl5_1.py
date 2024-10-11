d62ee30cac8fb83b0a8244c66c79cb73b0433a33
easyhardovich <easyhardovich@gmail.com>
2024-10-11 23:16:08 +0300

    doing home work for lesson 5.1

---
+++ hw_lesson_5/hwl5_1.py
@@ -0,0 +1,52 @@
+# Користувач вводить рядок. Ваше завдання - перевірити, чи може цей рядок бути ім'ям змінної.
+#
+# Змінна не може:
+#
+# починатися з цифри
+# містити великі літери,
+# пробіл і знаки пунктуації (взяти можна тут string.punctuation), окрім нижнього підкреслення "_".
+# бути жодним із зареєстрованих слів.
+# При цьому повне ім'я змінної повино складатись не більш чим з одного нижнього підкреслення "_".
+#
+# Список зареєстрованих слів можна взяти із keyword.kwlist.
+#
+# У результаті перевірки на друк виводиться або True, якщо таке ім'я змінної допустимо, або False - якщо ні.
+#
+# Приклади імен змінних та результат перевірки (=> на друк виводити не потрібно :))
+import string
+import keyword
+
+#список знаків пунктуації
+for_punctuation = string.punctuation
+#список зареєстрованих слів
+for_keywordlist = keyword.kwlist
+#запитуємо бажаний пароль у користувача
+user_row = str(input())
+#перевірка першого символу на число
+begin_digit = user_row[0].isdigit()
+#перевірка чи всі символи в нижньому регістрі
+lower_case = user_row.islower()
+
+#задаємо змінні для наступних перевірок
+is_punctuation = False
+lower_space = 0
+has_space = False
+is_keyword = False
+
+#цикл перевірки на знаки пунктуації, пробіл та кількість "_"
+for i in user_row:
+    if for_punctuation.find(i) >= 0 and i != "_":
+        is_punctuation = True
+    elif for_punctuation.find(i) >= 0:
+        lower_space += 1
+    elif  i == " ":
+        has_space = True
+
+#цикл перевірки приналежності до зареєстрованих слів
+for i in for_keywordlist:
+    if i == user_row:
+        is_keyword = True
+
+#True якщо всі True
+#Перший символ не цифра, тільки в нижньому регісті, не містить знаків пунктуації, "_" максимум 1, не містить пробілів, не зареєстроване слово
+print(all([begin_digit == False, lower_case == True, is_punctuation == False, lower_space <= 1, has_space == False, is_keyword == False]))
